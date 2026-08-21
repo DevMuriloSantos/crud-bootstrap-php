ee55f668778027ccdbdcdaefb9050b605c91365f
DevMuriloSantos <murilopaulo051@gmail.com>
2026-08-21 11:43:54 -0300

    feat: criação do sistema e layout de customers

--- inc/database.php
+++ inc/database.php
@@ -4,20 +4,72 @@ $mysqli = new mysqli_driver();
 $mysqli->report_mode = MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ERROR;
 //report_mode -> é um atributo;
 
-function open_database() {
+function open_database()
+{
 	try {
 		$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        $conn->set_charset("utf8"); // metodo;
+		$conn->set_charset("utf8"); // metodo;
 		return $conn;
 	} catch (Exception $e) {
 		throw new Exception("Erro ao conectar no banco de dados\n {$e->getMessage()}");
 	}
 }
 
-function close_database($conn) {
+function close_database($conn)
+{
 	try {
 		$conn->close();
 	} catch (Exception $e) {
 		throw new Exception("Erro ao encerrar conexão com o banco de dados\n {$e->getMessage()}");
 	}
+}
+
+/**
+ *  Pesquisa um Registro pelo ID em uma Tabela
+ */
+function find($table = null, $id = null)
+{
+	$found = null;
+
+	try {
+		$database = open_database();
+
+		if ($id) {
+			$sql = "SELECT * FROM $table WHERE id = $id";
+			$result = $database->query($sql);
+
+			if ($result->num_rows > 0) {
+				$found = $result->fetch_assoc();
+			}
+
+		} else {
+
+			$sql = "SELECT * FROM $table";
+			$result = $database->query($sql);
+
+			if ($result->num_rows > 0) {
+				$found = $result->fetch_all(MYSQLI_ASSOC);
+
+				/* Metodo alternativo
+				$found = [];
+				while ($row = $result->fetch_assoc()) {
+				  array_push($found, $row);
+				} */
+			}
+		}
+	} catch (Exception $e) {
+		$_SESSION['message'] = $e->GetMessage();
+		$_SESSION['type'] = 'danger';
+	}
+
+	close_database($database);
+	return $found;
+}
+
+/**
+ *  Pesquisa Todos os Registros de uma Tabela
+ */
+function find_all($table)
+{
+	return find($table);
 }
--- index.php
+++ index.php
@@ -1,47 +1,54 @@
-<?php 
-    include 'config.php';
-    include DBAPI;
+<?php
+include 'config.php';
+include DBAPI;
 
-    include HEADER_TEMPLATE;
-    $db = open_database(); 
+include HEADER_TEMPLATE;
+$erro = null;
+try {
+	$db = open_database();
+} catch (Exception $e) {
+	$erro = $e->getMessage();
+}
 ?>
 
 <h1>Dashboard</h1>
 <hr>
 
-<?php if ($db) : ?>
+<?php if (!$erro): ?>
 
-<div class="row">
-	<div class="col-xs-6 col-sm-3 col-md-2">
-		<a href="customers/add.php" class="btn btn-secondary">
-			<div class="row">
-				<div class="col-xs-12 text-center">
-					<i class="fa-solid fa-user-plus fa-5x"></i>
+	<div class="row">
+		<div class="col-xs-6 col-sm-3 col-md-2">
+			<a href="customers/add.php" class="btn btn-secondary">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<i class="fa-solid fa-user-plus fa-5x"></i>
+					</div>
+					<div class="col-xs-12 text-center">
+						<p>Novo Cliente</p>
+					</div>
 				</div>
-				<div class="col-xs-12 text-center">
-					<p>Novo Cliente</p>
+			</a>
+		</div>
+
+		<div class="col-xs-6 col-sm-3 col-md-2">
+			<a href="customers" class="btn btn-light">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<i class="fa fa-user-group fa-5x"></i>
+					</div>
+					<div class="col-xs-12 text-center">
+						<p>Clientes</p>
+					</div>
 				</div>
-			</div>
-		</a>
+			</a>
+		</div>
 	</div>
 
-	<div class="col-xs-6 col-sm-3 col-md-2">
-		<a href="customers" class="btn btn-light">
-			<div class="row">
-				<div class="col-xs-12 text-center">
-					<i class="fa fa-user-group fa-5x"></i>
-				</div>
-				<div class="col-xs-12 text-center">
-					<p>Clientes</p>
-				</div>
-			</div>
-		</a>
-	</div>
-</div>
-
-<?php else : ?>
+<?php else: ?>
 	<div class="alert alert-danger" role="alert">
-		<p><b>ERRO:</b> Não foi possível Conectar ao Banco de Dados!</p>
+		<p><b>ERRO:</b> Não foi possível Conectar ao Banco de Dados!<br>
+			<?= $erro ?>
+		</p>
 	</div>
 
 <?php endif; ?>
